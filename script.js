6f246cce8f1407c4f60f3edb030b3f398e6676bc
JoanCarlos10 <jcfrg10@gmail.com>
2025-08-28 12:39:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -407,14 +407,14 @@ menu.forEach(dia => {
   doc.setFontSize(12);
   doc.text(dia.dia, x, y += 18);   // título del día
   doc.setFont("helvetica","normal");
-  y += 10;                          // <-- margen bajo el título
+  y += 15;                          // <-- margen bajo el título
 
   y = wrap(doc, `Esmorzar: ${dia.esmorzar}`, x, y, maxW);
   y = wrap(doc, `Dinar: ${dia.dinar}`,     x, y, maxW);
   y = wrap(doc, `Sopar: ${dia.sopar}`,     x, y, maxW);
   y = wrap(doc, `Snack: ${dia.snack}`,     x, y, maxW);
 
-  y += 15;                          // espacio entre días
+  y += 10;                          // espacio entre días
 });
 
 
@@ -598,6 +598,7 @@ menu.forEach(dia => {
     doc.save(fname);
   });
 })();
+
 
 
 
